ee1dc678139c1c55e1f0d93275adb76e89847616
Iris <iris.teyssier@gmail.com>
2024-02-14 21:25:25 +0000

    partiallly implement triplet searching

--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,8 @@ pub fn run(threads: Option<i32>, strict: bool) {
     let num_threads = if let Some(num) = threads { num } else { 8 };
 
     let paused = Arc::new(Mutex::new(true));
+    let triplets = Arc::new(Mutex::new(Vec::new()));
+    let current = Arc::new(Mutex::new(0u64));
     let mut handles = vec![];
 
     for _ in 0..num_threads {
@@ -103,11 +105,40 @@ where
     }
 }
 
-fn check(paused: Arc<Mutex<bool>>) {
+fn check(paused: Arc<Mutex<bool>>, current: Arc<Mutex<u64>>, triplets: Arc<Mutex<Vec<Triplet>>>) {
     loop {
         if !*paused.lock().unwrap() {
-            println!("Hi!");
-            thread::sleep(Duration::from_millis(1000));
+            // get the next number and increment the counter
+            
+            // check all possible variations
+
+            // if one is found, check that it is simplified, then check if it already is in the vec
+            // if not, add it to it
+
+            let current = current.lock().unwrap();
+            *current += 1; // increment current, so multiple threads don't check the same number
+            let c = *current;
+            drop(current); // unlock the mutex
+
+            for a in 1..(c + 1) {
+                for b in 1..(a + 1) {
+                    if a.pow(2) + b.pow(2) == c.pow(2) {
+                        let triplet = Triplet::new(b, a, c);
+                        // we only lock triplets when we need to check it
+                        let mut triplets = *triplets.lock().unwrap();
+                        // if !contains_triplet(&triplets, &triple) {
+                        if !contains_triplet(&triplets, &triplet) {
+                            triplets.push(triplet);
+    
+                            // file //TODO: implement display for triplet
+                            // .write_fmt(format_args!("{}, {}, {}\n", b, a, c))
+                            // .expect("failed to write to file");
+                            // println!("{}, {}, {}", b, a, c);
+                        }
+                    }
+                }
+            }
+            
         }
     }
 }
@@ -125,15 +156,17 @@ impl Triplet {
     }
 }
 
-fn contains_triplet(arr: &[Triplet], triple: &Triplet) -> bool {
+fn contains_triplet(triplets: &[Triplet], triplet: &Triplet) -> bool {
     for i in arr {
-        if (((triple.a % i.a == 0) && (triple.b % i.b == 0))
-            || ((triple.a % i.b == 0) && (triple.b % i.a == 0)))
-            && (triple.c % i.c == 0)
+        if (((triplet.a % i.a == 0) && (triplet.b % i.b == 0))
+            || ((triplet.a % i.b == 0) && (triplet.b % i.a == 0)))
+            && (triplet.c % i.c == 0)
         {
             return true;
         }
     }
+
+    // TODO: simplify the triplet, then use .contains
 
     false
 }
