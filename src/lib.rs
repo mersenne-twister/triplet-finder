8ef16a9d0ecbe85b8e5880ee8fd35387741520c2
Iris Teyssier <iris.teyssier@gmail.com>
2024-02-14 07:08:32 -0800

    add plan

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,62 @@
+// temporary, to make it easier to see the important errors
+#![allow(dead_code, unused_variables, unused_imports)]
+
 mod intro;
 
-use std::{fs::File, io::Write/*, process*/};
+use std::{
+    fs::File,
+    io,
+    io::Write,
+    sync::{Arc, Mutex},
+    thread,
+    time::Duration,
+};
 
 pub fn run(threads: Option<i32>, strict: bool) {
     println!("{}", intro::MESSAGE);
-    // let mut file = File::create("triplets").unwrap_or_else(|err| {
-    //     panic!("Could not create file: {}", err);
-    // });
 
-    // let mut triplets = Vec::new();
+    let num_threads = if let Some(num) = threads { num } else { 8 };
 
-    // for c in 1u64..=50 {
-    //     for a in 1..(c + 1) {
-    //         for b in 1..(a + 1) {
-    //             if a.pow(2) + b.pow(2) == c.pow(2) {
-    //                 let triple = Triplet::new(b, a, c);
+    // create mutices for paused, triplets (Vec<Triplet>)
 
-    //                 if !contains_triple(&triplets, &triple) {
-    //                     triplets.push(triple);
+    // spawn threads
+    // if !paused
+    // get the next number
+    // compute it
+    // if triplet is found, check it, and if it's simplified,
+    // check found_triplets, and if it's new, push it to found
 
-    //                     file //TODO: implement display for triplet
-    //                     .write_fmt(format_args!("{}, {}, {}\n", b, a, c)) 
-    //                     .expect("failed to write to file");
-    //                     println!("{}, {}, {}", b, a, c);
-    //                 }
-    //             }
-    //         }
-    //     }
-    // }
+    // loop for input
+    // if exit, close threads
 
-    // println!("All triplets up to 18446744073709551615 found.");
+    /*
+    let mut file = File::create("triplets").unwrap_or_else(|err| {
+        panic!("Could not create file: {}", err);
+    });
+
+    let mut triplets = Vec::new();
+
+    for c in 1u64..=50 {
+        for a in 1..(c + 1) {
+            for b in 1..(a + 1) {
+                if a.pow(2) + b.pow(2) == c.pow(2) {
+                    let triple = Triplet::new(b, a, c);
+
+                    if !contains_triple(&triplets, &triple) {
+                        triplets.push(triple);
+
+                        file //TODO: implement display for triplet
+                        .write_fmt(format_args!("{}, {}, {}\n", b, a, c))
+                        .expect("failed to write to file");
+                        println!("{}, {}, {}", b, a, c);
+                    }
+                }
+            }
+        }
+    }
+
+    println!("All triplets up to 18446744073709551615 found.");
+    */
 }
 
 #[derive(PartialEq, Debug)]
@@ -45,8 +72,7 @@ impl Triplet {
     }
 }
 
-
-fn contains_triple(arr: &[Triplet], triple: &Triplet) -> bool {
+fn contains_triplet(arr: &[Triplet], triple: &Triplet) -> bool {
     for i in arr {
         if (((triple.a % i.a == 0) && (triple.b % i.b == 0))
             || ((triple.a % i.b == 0) && (triple.b % i.a == 0)))
@@ -63,7 +89,7 @@ fn contains_triple(arr: &[Triplet], triple: &Triplet) -> bool {
 mod test {
     #[test]
     fn foo() {
-        assert!(contains_triple(
+        assert!(contains_triplet(
             &[Triplet::new(3, 4, 5)],
             &Triplet::new(6, 8, 10)
         ));
