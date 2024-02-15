0f63facdde16ad0a92ff17a064871a437a4b3584
Iris Teyssier <iris.teyssier@gmail.com>
2024-02-14 23:57:03 -0800

    add broken loading support

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,8 @@ mod intro;
 
 use core::num;
 use std::{
-    fs::File,
+    error::Error,
+    fs::{self, File},
     hint::spin_loop,
     io::{self, Write},
     sync::{Arc, Mutex, RwLock},
@@ -21,7 +22,7 @@ impl Stop {
     fn new() -> Self {
         Self {
             stop: false,
-            stopped: 0
+            stopped: 0,
         }
     }
 }
@@ -42,7 +43,7 @@ pub fn run(threads: Option<u32>, strict: bool) {
         let current = Arc::clone(&current);
         let triplets = Arc::clone(&triplets);
         let stop = Arc::clone(&stop);
-        
+
         thread::spawn(move || find_triplets(paused, current, triplets, stop));
     }
 
@@ -51,7 +52,7 @@ pub fn run(threads: Option<u32>, strict: bool) {
         let num_triplets = triplets.lock().unwrap().len();
         let triplets = Arc::clone(&triplets);
         let print = Arc::clone(&print);
-        
+
         thread::spawn(move || print_triplets(triplets, num_triplets, print));
     }
 
@@ -60,15 +61,11 @@ pub fn run(threads: Option<u32>, strict: bool) {
         input.clear();
         io::stdin().read_line(&mut input).unwrap();
 
-        // println!("current: {}", current.lock().unwrap());
+        if strict {
+            input = input.to_ascii_lowercase();
+        }
 
-        match input
-            .to_lowercase()
-            .trim()
-            .split_ascii_whitespace()
-            .next()
-            .unwrap_or("")
-        {
+        match input.trim().split_ascii_whitespace().next().unwrap_or("") {
             "start" => {
                 *paused.write().unwrap() = false;
                 println!("Program executing.")
@@ -100,29 +97,52 @@ pub fn run(threads: Option<u32>, strict: bool) {
                 }
             }
             "save" => run_if_paused(&paused, || {
+                let arg = input
+                    .split_ascii_whitespace()
+                    .nth(1)
+                    .unwrap_or("triplets.txt");
+
+                let mut file = match File::create(arg) {
+                    Ok(file) => file,
+                    Err(err) => {
+                        println!("{}\nError-type: {}", intro::SAVE_ERROR, err);
+                        return;
+                    }
+                };
+
                 // don't print new triplets found, as we're closing the threads
                 *print.write().unwrap() = false;
                 // necessary because it would otherwise never get to the stopping point
                 *paused.write().unwrap() = false;
                 stop.write().unwrap().stop = true;
                 println!("Suspending threads...");
-                while stop.read().unwrap().stopped < num_threads {
-                    // thread::sleep(Duration::from_secs(1));
-                    // println!("num_stopped: {}", stop.read().unwrap().stopped);
-                }
+                while stop.read().unwrap().stopped < num_threads {}
                 // after everything has stopped, re-pause
                 *paused.write().unwrap() = true;
                 println!("Threads suspended.\nSaving data...");
 
-                // save data somehow
+                file.write_fmt(format_args!("{}\n", current.lock().unwrap()))
+                    .unwrap();
+                for triplet in (*triplets.lock().unwrap()).iter() {
+                    file.write_fmt(format_args!("{}-{}-{}\n", triplet.a, triplet.b, triplet.c))
+                        .unwrap();
+                }
 
-                println!("Data saved.");
+                println!("Data saved to {}.", arg);
                 stop.write().unwrap().stop = false;
                 // reset the counter
                 stop.write().unwrap().stopped = 0;
             }),
             "load" => run_if_paused(&paused, || {
-                todo!();
+                // try to load the file, and error if it fails
+                let arg = input
+                    .split_ascii_whitespace()
+                    .nth(1)
+                    .unwrap_or("triplets.txt");
+
+                load(arg, &current, &triplets).unwrap_or_else(|err| {
+                    println!("Load error, file possibly corrupted.\nError type: {}", err);
+                })
             }),
             "help" => println!("{}", intro::HELP),
             "" => (), // don't show an error if they don't input anything
@@ -155,10 +175,42 @@ where
     }
 }
 
-/// TODO: move input into here
-fn input() {
+fn load(
+    arg: &str,
+    current: &Arc<Mutex<u64>>,
+    triplets: &Arc<Mutex<Vec<Triplet>>>,
+) -> Result<(), Box<dyn Error>> {
+    // let content = match fs::read_to_string(arg) {
+    //     Ok(content) => content.lines(),
+    //     Err(err) => {
+    //         println!("{}\nError-type: {}", intro::LOAD_ERROR, err);
+    //         return;
+    //     }
+    // };
 
+    let content = fs::read_to_string(arg)?;
+    let mut content = content.lines();
+
+    *current.lock().unwrap() = content.next().ok_or("File corrupted")?.parse()?;
+
+    let mut triplets = triplets.lock().unwrap();
+    let mut nums = Vec::new();
+    for triplet in content {
+        nums.clear();
+        for num in triplet.split('-') {
+            nums.push(num.parse()?);
+        }
+        if nums.len() < 3 {
+            return Err("File corrupted".into());
+        }
+
+        triplets.push(Triplet::new(nums[0], nums[1], nums[2]));
+    }
+    Ok(())
 }
+
+/// TODO: move input into here
+fn input() {}
 
 fn print_triplets(
     triplets: Arc<Mutex<Vec<Triplet>>>,
@@ -185,16 +237,9 @@ fn find_triplets(
     paused: Arc<RwLock<bool>>,
     current: Arc<Mutex<u64>>,
     triplets: Arc<Mutex<Vec<Triplet>>>,
-    stop: Arc<RwLock<Stop>>
+    stop: Arc<RwLock<Stop>>,
 ) {
     loop {
-        // get the next number and increment the counter
-
-        // check all possible variations
-
-        // if one is found, check that it is simplified, then check if it already is in the vec
-        // if not, add it to it
-
         let c = {
             let mut current = current.lock().unwrap();
             *current += 1; // increment current, so multiple threads don't check the same number
@@ -212,15 +257,11 @@ fn find_triplets(
                     // if !contains_triplet(&triplets, &triple) {
                     if !contains_triplet(&triplets, &triplet) {
                         triplets.push(triplet);
-
-                        // file //TODO: implement display for triplet
-                        // .write_fmt(format_args!("{}, {}, {}\n", b, a, c))
-                        // .expect("failed to write to file");
-                        // println!("{}, {}, {}", b, a, c);
                     }
                 }
             }
         }
+
         if stop.read().unwrap().stop {
             stop.write().unwrap().stopped += 1;
             while stop.read().unwrap().stop {}
