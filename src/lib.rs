50cdf0311151be2a825cffb91e6e8013d1a14198
Iris Teyssier <iris.teyssier@gmail.com>
2024-02-14 19:53:51 -0800

    implement saving

--- src/lib.rs
+++ src/lib.rs
@@ -13,13 +13,27 @@ use std::{
     time::Duration,
 };
 
-pub fn run(threads: Option<i32>, strict: bool) {
+struct Stop {
+    stop: bool,
+    stopped: u32,
+}
+impl Stop {
+    fn new() -> Self {
+        Self {
+            stop: false,
+            stopped: 0
+        }
+    }
+}
+
+pub fn run(threads: Option<u32>, strict: bool) {
     println!("{}{}", intro::MESSAGE, intro::HELP);
 
     let num_threads = if let Some(num) = threads { num } else { 8 };
 
     // TODO: use `RwLock` for `paused`
-    let paused = Arc::new(Mutex::new(true));
+    let paused = Arc::new(RwLock::new(true));
+    let stop = Arc::new(RwLock::new(Stop::new()));
     let print = Arc::new(RwLock::new(true));
     let triplets = Arc::new(Mutex::new(Vec::new()));
     let current = Arc::new(Mutex::new(0u64));
@@ -28,13 +42,17 @@ pub fn run(threads: Option<i32>, strict: bool) {
         let paused = Arc::clone(&paused);
         let current = Arc::clone(&current);
         let triplets = Arc::clone(&triplets);
-        thread::spawn(move || check(paused, current, triplets));
+        let stop = Arc::clone(&stop);
+        
+        thread::spawn(move || find_triplets(paused, current, triplets, stop));
     }
 
-    { // spawn the thread to print the values as they're found
+    {
+        // spawn the thread to print the values as they're found
         let num_triplets = triplets.lock().unwrap().len();
         let triplets = Arc::clone(&triplets);
         let print = Arc::clone(&print);
+        
         thread::spawn(move || print_triplets(triplets, num_triplets, print));
     }
 
@@ -43,17 +61,35 @@ pub fn run(threads: Option<i32>, strict: bool) {
         input.clear();
         io::stdin().read_line(&mut input).unwrap();
 
-        match input.to_lowercase().trim().split_ascii_whitespace().next().unwrap() {
+        // println!("current: {}", current.lock().unwrap());
+
+        match input
+            .to_lowercase()
+            .trim()
+            .split_ascii_whitespace()
+            .next()
+            .unwrap_or("")
+        {
             "start" => {
-                *paused.lock().unwrap() = false;
+                *paused.write().unwrap() = false;
                 println!("Program executing.")
             }
             "stop" => {
-                *paused.lock().unwrap() = true;
+                *paused.write().unwrap() = true;
                 println!("Program suspended.")
             }
             "print" => {
-                println!("{}", input.split_ascii_whitespace().nth(1).unwrap_or(intro::PRINT_ERROR));
+                let arg = input
+                    .split_ascii_whitespace()
+                    .nth(1)
+                    .unwrap_or("")
+                    .parse::<bool>();
+                if let Ok(arg) = arg {
+                    *print.write().unwrap() = arg;
+                    println!("Printing {}.", if arg { "enabled" } else { "disabled" })
+                } else {
+                    println!("{}", intro::PRINT_ERROR);
+                }
             }
             "exit" => {
                 let mut ret = false;
@@ -65,7 +101,16 @@ pub fn run(threads: Option<i32>, strict: bool) {
                 }
             }
             "save" => run_if_paused(&paused, || {
-                todo!();
+                // don't print new triplets found, as we're closing the threads
+                *print.write().unwrap() = false;
+                stop.write().unwrap().stop = true;
+                println!("Stopping threads...");
+                while stop.read().unwrap().stopped < num_threads {}
+                println!("Threads stopped.\nSaving data...");
+
+                // save data somehow
+
+
             }),
             "load" => run_if_paused(&paused, || {
                 todo!();
@@ -89,63 +134,87 @@ pub fn run(threads: Option<i32>, strict: bool) {
     // if exit, close threads
 }
 
-fn run_if_paused<F>(paused: &Arc<Mutex<bool>>, f: F)
+/// TODO: find a better way to do this
+fn run_if_paused<F>(paused: &Arc<RwLock<bool>>, f: F)
 where
     F: FnOnce(),
 {
-    if *paused.lock().unwrap() {
+    if *paused.read().unwrap() {
         f();
     } else {
         println!("{}", intro::RUNNING_ERROR);
     }
 }
 
-fn print_triplets(triplets: Arc<Mutex<Vec<Triplet>>>, starting_size: usize, print: Arc<RwLock<bool>>) {
-    let num_printed = starting_size;
+/// TODO: move input into here
+fn input() {
+
+}
+
+fn print_triplets(
+    triplets: Arc<Mutex<Vec<Triplet>>>,
+    starting_size: usize,
+    print: Arc<RwLock<bool>>,
+) {
+    let mut num_printed = starting_size;
     loop {
+        if !*print.read().unwrap() {
+            continue;
+        }
         let num_found = (*triplets.lock().unwrap()).len();
         if num_found > num_printed {
             for num in num_printed..num_found {
                 let triplet = &(*triplets.lock().unwrap())[num];
                 println!("{}-{}-{}", triplet.a, triplet.b, triplet.c);
             }
+            num_printed = num_found;
         }
     }
 }
 
-fn check(paused: Arc<Mutex<bool>>, current: Arc<Mutex<u64>>, triplets: Arc<Mutex<Vec<Triplet>>>) {
+fn find_triplets(
+    paused: Arc<RwLock<bool>>,
+    current: Arc<Mutex<u64>>,
+    triplets: Arc<Mutex<Vec<Triplet>>>,
+    stop: Arc<RwLock<Stop>>
+) {
     loop {
-        if !*paused.lock().unwrap() {
-            // get the next number and increment the counter
+        // get the next number and increment the counter
 
-            // check all possible variations
+        // check all possible variations
 
-            // if one is found, check that it is simplified, then check if it already is in the vec
-            // if not, add it to it
+        // if one is found, check that it is simplified, then check if it already is in the vec
+        // if not, add it to it
 
+        let c = {
             let mut current = current.lock().unwrap();
             *current += 1; // increment current, so multiple threads don't check the same number
-            let c = *current;
-            drop(current); // unlock the mutex
+            *current
+        };
 
-            for a in 1..(c + 1) {
-                for b in 1..(a + 1) {
-                    if a.pow(2) + b.pow(2) == c.pow(2) {
-                        let triplet = Triplet::new(b, a, c);
-                        // we only lock triplets when we need to check it
-                        let mut triplets = triplets.lock().unwrap();
-                        // if !contains_triplet(&triplets, &triple) {
-                        if !contains_triplet(&triplets, &triplet) {
-                            triplets.push(triplet);
+        for a in 1..=c {
+            for b in 1..=a {
+                while *paused.read().unwrap() {}
 
-                            // file //TODO: implement display for triplet
-                            // .write_fmt(format_args!("{}, {}, {}\n", b, a, c))
-                            // .expect("failed to write to file");
-                            // println!("{}, {}, {}", b, a, c);
-                        }
+                if a.pow(2) + b.pow(2) == c.pow(2) {
+                    let triplet = Triplet::new(b, a, c);
+                    // we only lock triplets when we need to check it
+                    let mut triplets = triplets.lock().unwrap();
+                    // if !contains_triplet(&triplets, &triple) {
+                    if !contains_triplet(&triplets, &triplet) {
+                        triplets.push(triplet);
+
+                        // file //TODO: implement display for triplet
+                        // .write_fmt(format_args!("{}, {}, {}\n", b, a, c))
+                        // .expect("failed to write to file");
+                        // println!("{}, {}, {}", b, a, c);
                     }
                 }
             }
+        }
+        if stop.read().unwrap().stop {
+            stop.write().unwrap().stopped += 1;
+            while stop.read().unwrap().stop {}
         }
     }
 }
