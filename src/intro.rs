14fc833f0ba31d4d741315580af90c4650eb790d
Iris Teyssier <iris.teyssier@gmail.com>
2024-02-14 23:56:51 -0800

    add error messages for saving

--- src/intro.rs
+++ src/intro.rs
@@ -17,10 +17,13 @@ To enable/disable the printing of triplets, enter `print [bool]`
 While paused, it is possible to do save/load operations, and to exit
 To exit, enter `exit`
 To save to a file, enter `save [filename]`.
+    (default: `triplets.txt`)
     (For maximum success rate, enter a single phase, hyphen-seperated (like that!) if applicable.
     Anything else is liable to crash the program)
-    (triplets will be ordered by hypotenuse, ascending)
+    (triplets will be ordered by hypotenuse, ascending order)
+    (this operation will overwrite any previous contents of the file, so be careful!)
 To load from a file, enter `load [filename]`.
+    (default: `triplets.txt`)
     (same advice as above)
 
 To see see this message, enter `help`.
@@ -29,7 +32,7 @@ ADVANCED:
     To change the number of working threads, use the `--threads [int]` flag.
 
 Note that the first line of the file is used for program info. If it is deleted
-or modified in any way, the program will be unable to load it.
+or modified, the file may become corrupted.
 
 Also note that this program is very computationally intensive, so it is reccomended to
 only run it when not running other intensive applications.
@@ -47,3 +50,11 @@ Cannot perform this task while program running.
 To suspend execution, enter `stop`.";
 
 pub const PRINT_ERROR: &str = "Invalid arguement. Syntax of command is `print [bool]`.";
+
+pub const SAVE_ERROR: &str = "\
+Invalid filename. Syntax of command is `save [filename]`,
+subject to your os's whims.";
+
+pub const LOAD_ERROR: &str = "\
+Invalid filename. Syntax of command is `load [filename]`,
+subject to your os's whims.";
