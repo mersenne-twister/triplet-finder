803c24ae2b91398151571095dc9c36c8d730291a
Iris Teyssier <iris.teyssier@gmail.com>
2024-02-15 21:49:03 -0800

    move input functions to input module

---
+++ src/input.rs
@@ -0,0 +1,167 @@
+use {
+    crate::{input, text, Stop, Triplet},
+    std::{
+        error::Error,
+        fs::{self, File},
+        io::{self, Write},
+        sync::{Arc, Mutex, RwLock},
+    },
+};
+
+pub fn save(
+    arg: &str,
+    num_threads: u32,
+    print: &Arc<RwLock<bool>>,
+    paused: &Arc<RwLock<bool>>,
+    stop: &Arc<RwLock<Stop>>,
+    current: &Arc<Mutex<u64>>,
+    triplets: &Arc<Mutex<Vec<Triplet>>>,
+) -> Result<(), Box<dyn Error>> {
+    println!("Opening file...");
+    let mut file = File::create(arg)?;
+
+    // TODO: move this to function stop_threads
+    // don't print new triplets found, as we're closing the threads
+    *print.write().unwrap() = false;
+    // necessary because it would otherwise never get to the stopping point
+    *paused.write().unwrap() = false;
+    stop.write().unwrap().stop = true;
+    println!("Suspending threads...");
+    while stop.read().unwrap().stopped < num_threads {}
+    // after everything has stopped, re-pause
+    *paused.write().unwrap() = true;
+    println!("Saving data...");
+
+    file.write_fmt(format_args!("{}\n", current.lock().unwrap()))?;
+    for triplet in (*triplets.lock().unwrap()).iter() {
+        file.write_fmt(format_args!("{}-{}-{}\n", triplet.a, triplet.b, triplet.c))?;
+    }
+
+    println!("Data saved to {}.", arg);
+    stop.write().unwrap().stop = false;
+    // reset the counter
+    stop.write().unwrap().stopped = 0;
+
+    Ok(())
+}
+
+/// routine to load state from a file
+pub fn load(
+    arg: &str,
+    current: &Arc<Mutex<u64>>,
+    triplets: &Arc<Mutex<Vec<Triplet>>>,
+    print: &Arc<RwLock<bool>>,
+    init: &Arc<RwLock<Option<usize>>>,
+) -> Result<(), Box<dyn Error>> {
+    println!("Reading {}...", arg);
+    let content = fs::read_to_string(arg)?;
+    println!("Loading contents...");
+    let mut content = content.lines();
+
+    *current.lock().unwrap() = content.next().ok_or("File corrupted")?.parse()?;
+    let print_state = *print.read().unwrap();
+    *print.write().unwrap() = false;
+
+    let mut triplets = triplets.lock().unwrap();
+    let mut nums = Vec::new();
+    for triplet in content {
+        nums.clear();
+        for num in triplet.split('-') {
+            nums.push(num.parse()?);
+        }
+        if nums.len() < 3 {
+            return Err("File corrupted".into());
+        }
+
+        triplets.push(Triplet::new(nums[0], nums[1], nums[2]));
+    }
+
+    println!("Successfully loaded from {}.", arg);
+
+    *init.write().unwrap() = Some(triplets.len());
+    *print.write().unwrap() = print_state;
+
+    Ok(())
+}
+
+/// TODO: move input into here
+pub fn input(
+    strict: bool,
+    paused: Arc<RwLock<bool>>,
+    print: Arc<RwLock<bool>>,
+    num_threads: u32,
+    stop: Arc<RwLock<Stop>>,
+    current: Arc<Mutex<u64>>,
+    triplets: Arc<Mutex<Vec<Triplet>>>,
+    print_init: Arc<RwLock<Option<usize>>>,
+) {
+    let mut input = String::new();
+    loop {
+        input.clear();
+        io::stdin().read_line(&mut input).unwrap();
+
+        if strict {
+            input = input.to_ascii_lowercase();
+        }
+
+        match input.trim().split_ascii_whitespace().next().unwrap_or("") {
+            "start" => {
+                *paused.write().unwrap() = false;
+                println!("Program executing.")
+            }
+            "stop" => {
+                *paused.write().unwrap() = true;
+                println!("Program suspended.")
+            }
+            "print" => {
+                let arg = input
+                    .split_ascii_whitespace()
+                    .nth(1)
+                    .unwrap_or("")
+                    .parse::<bool>();
+                if let Ok(arg) = arg {
+                    *print.write().unwrap() = arg;
+                    println!("Printing {}.", if arg { "enabled" } else { "disabled" })
+                } else {
+                    println!("{}", text::PRINT_ERROR);
+                }
+            }
+            "exit" if *paused.read().unwrap() => return,
+            "save" if *paused.read().unwrap() => {
+                let arg = input
+                    .split_ascii_whitespace()
+                    .nth(1)
+                    .unwrap_or("triplets.txt");
+
+                input::save(
+                    arg,
+                    num_threads,
+                    &print,
+                    &paused,
+                    &stop,
+                    &current,
+                    &triplets,
+                )
+                .unwrap_or_else(|err| {
+                    println!("Save error: {}.", arg);
+                })
+            }
+            "load" if *paused.read().unwrap() => {
+                // try to load the file, and error if it fails
+                let arg = input
+                    .split_ascii_whitespace()
+                    .nth(1)
+                    .unwrap_or("triplets.txt");
+
+                input::load(arg, &current, &triplets, &print, &print_init).unwrap_or_else(|err| {
+                    println!("Load error: {}.\nFile possibly corrupted.", err);
+                });
+            }
+            // show an error instead of silently denying
+            "exit" | "save" | "load" => println!("{}", text::RUNNING_ERROR),
+            "help" => println!("{}", text::HELP),
+            "" => (), // don't show an error if they don't input anything
+            _ => println!("{}", text::INPUT_ERROR),
+        }
+    }
+}
