ec6cabc63c982edf469c2636efc103b24ab2bd29
Iris Teyssier <iris.teyssier@gmail.com>
2024-02-15 22:06:10 -0800

    use find struct everywhere

--- src/find.rs
+++ src/find.rs
@@ -1,11 +1,11 @@
+use crate::Stop;
 use std::sync::{Arc, Mutex, RwLock};
-use crate::{Stop, Triplet};
 
 pub struct Find {
-    paused: RwLock<bool>,
-    triplets: Mutex<Vec<Triplet>>,
-    current: Mutex<u64>,
-    stop: RwLock<Stop>,
+    pub paused: RwLock<bool>,
+    pub triplets: Mutex<Vec<Triplet>>,
+    pub current: Mutex<u64>,
+    pub stop: RwLock<Stop>,
 }
 impl Find {
     pub fn new() -> Arc<Self> {
@@ -13,7 +13,7 @@ impl Find {
             paused: RwLock::new(true),
             triplets: Mutex::new(Vec::new()),
             current: Mutex::new(0u64),
-            stop:RwLock::new(Stop::new()),
+            stop: RwLock::new(Stop::new()),
         })
     }
 
@@ -24,28 +24,55 @@ impl Find {
                 *current += 1; // increment current, so multiple threads don't check the same number
                 *current
             };
-    
+
             for a in 1..=c {
                 for b in 1..=a {
                     while *self.paused.read().unwrap() {}
-    
+
                     if a.pow(2) + b.pow(2) == c.pow(2) {
                         let triplet = Triplet::new(b, a, c);
                         // we only lock triplets when we need to check it
                         let mut triplets = self.triplets.lock().unwrap();
-                        // if !contains_triplet(&triplets, &triple) {
-                        if !contains_triplet(&triplets, &triplet) {
+                        // if !contains_triplet(&triplets, &triplet) {
+                        if !triplet.contained_in(&triplets) {
                             triplets.push(triplet);
                         }
                     }
                 }
             }
-    
-            if stop.read().unwrap().stop {
-                stop.write().unwrap().stopped += 1;
-                while stop.read().unwrap().stop {}
+
+            if self.stop.read().unwrap().stop {
+                self.stop.write().unwrap().stopped += 1;
+                while self.stop.read().unwrap().stop {}
             }
         }
     }
 }
 
+#[derive(PartialEq, Debug)]
+pub struct Triplet {
+    pub a: u64,
+    pub b: u64,
+    pub c: u64,
+}
+
+impl Triplet {
+    pub fn new(a: u64, b: u64, c: u64) -> Triplet {
+        Triplet { a, b, c }
+    }
+
+    fn contained_in(&self, triplets: &[Triplet]) -> bool {
+        for i in triplets {
+            if (((self.a % i.a == 0) && (self.b % i.b == 0))
+                || ((self.a % i.b == 0) && (self.b % i.a == 0)))
+                && (self.c % i.c == 0)
+            {
+                return true;
+            }
+        }
+
+        // TODO: simplify the triplet, then use .contains
+
+        false
+    }
+}
--- src/input.rs
+++ src/input.rs
@@ -1,5 +1,5 @@
 use {
-    crate::{input, text, Stop, Triplet},
+    crate::{find::{Find, Triplet}, input, text, Stop},
     std::{
         error::Error,
         fs::{self, File},
@@ -12,10 +12,7 @@ pub fn save(
     arg: &str,
     num_threads: u32,
     print: &Arc<RwLock<bool>>,
-    paused: &Arc<RwLock<bool>>,
-    stop: &Arc<RwLock<Stop>>,
-    current: &Arc<Mutex<u64>>,
-    triplets: &Arc<Mutex<Vec<Triplet>>>,
+    find: Arc<Find>
 ) -> Result<(), Box<dyn Error>> {
     println!("Opening file...");
     let mut file = File::create(arg)?;
@@ -24,23 +21,23 @@ pub fn save(
     // don't print new triplets found, as we're closing the threads
     *print.write().unwrap() = false;
     // necessary because it would otherwise never get to the stopping point
-    *paused.write().unwrap() = false;
-    stop.write().unwrap().stop = true;
+    *find.paused.write().unwrap() = false;
+    find.stop.write().unwrap().stop = true;
     println!("Suspending threads...");
-    while stop.read().unwrap().stopped < num_threads {}
+    while find.stop.read().unwrap().stopped < num_threads {}
     // after everything has stopped, re-pause
-    *paused.write().unwrap() = true;
+    *find.paused.write().unwrap() = true;
     println!("Saving data...");
 
-    file.write_fmt(format_args!("{}\n", current.lock().unwrap()))?;
-    for triplet in (*triplets.lock().unwrap()).iter() {
+    file.write_fmt(format_args!("{}\n", find.current.lock().unwrap()))?;
+    for triplet in (*find.triplets.lock().unwrap()).iter() {
         file.write_fmt(format_args!("{}-{}-{}\n", triplet.a, triplet.b, triplet.c))?;
     }
 
     println!("Data saved to {}.", arg);
-    stop.write().unwrap().stop = false;
+    find.stop.write().unwrap().stop = false;
     // reset the counter
-    stop.write().unwrap().stopped = 0;
+    find.stop.write().unwrap().stopped = 0;
 
     Ok(())
 }
@@ -48,21 +45,20 @@ pub fn save(
 /// routine to load state from a file
 pub fn load(
     arg: &str,
-    current: &Arc<Mutex<u64>>,
-    triplets: &Arc<Mutex<Vec<Triplet>>>,
     print: &Arc<RwLock<bool>>,
     init: &Arc<RwLock<Option<usize>>>,
+    find: Arc<Find>
 ) -> Result<(), Box<dyn Error>> {
     println!("Reading {}...", arg);
     let content = fs::read_to_string(arg)?;
     println!("Loading contents...");
     let mut content = content.lines();
 
-    *current.lock().unwrap() = content.next().ok_or("File corrupted")?.parse()?;
+    *find.current.lock().unwrap() = content.next().ok_or("File corrupted")?.parse()?;
     let print_state = *print.read().unwrap();
     *print.write().unwrap() = false;
 
-    let mut triplets = triplets.lock().unwrap();
+    let mut triplets = find.triplets.lock().unwrap();
     let mut nums = Vec::new();
     for triplet in content {
         nums.clear();
@@ -87,13 +83,10 @@ pub fn load(
 /// TODO: move input into here
 pub fn input(
     strict: bool,
-    paused: Arc<RwLock<bool>>,
     print: Arc<RwLock<bool>>,
     num_threads: u32,
-    stop: Arc<RwLock<Stop>>,
-    current: Arc<Mutex<u64>>,
-    triplets: Arc<Mutex<Vec<Triplet>>>,
     print_init: Arc<RwLock<Option<usize>>>,
+    find: Arc<Find>,
 ) {
     let mut input = String::new();
     loop {
@@ -106,11 +99,11 @@ pub fn input(
 
         match input.trim().split_ascii_whitespace().next().unwrap_or("") {
             "start" => {
-                *paused.write().unwrap() = false;
+                *find.paused.write().unwrap() = false;
                 println!("Program executing.")
             }
             "stop" => {
-                *paused.write().unwrap() = true;
+                *find.paused.write().unwrap() = true;
                 println!("Program suspended.")
             }
             "print" => {
@@ -126,8 +119,8 @@ pub fn input(
                     println!("{}", text::PRINT_ERROR);
                 }
             }
-            "exit" if *paused.read().unwrap() => return,
-            "save" if *paused.read().unwrap() => {
+            "exit" if *find.paused.read().unwrap() => return,
+            "save" if *find.paused.read().unwrap() => {
                 let arg = input
                     .split_ascii_whitespace()
                     .nth(1)
@@ -137,23 +130,20 @@ pub fn input(
                     arg,
                     num_threads,
                     &print,
-                    &paused,
-                    &stop,
-                    &current,
-                    &triplets,
+                    find
                 )
                 .unwrap_or_else(|err| {
                     println!("Save error: {}.", arg);
                 })
             }
-            "load" if *paused.read().unwrap() => {
+            "load" if *find.paused.read().unwrap() => {
                 // try to load the file, and error if it fails
                 let arg = input
                     .split_ascii_whitespace()
                     .nth(1)
                     .unwrap_or("triplets.txt");
 
-                input::load(arg, &current, &triplets, &print, &print_init).unwrap_or_else(|err| {
+                input::load(arg,  &print, &print_init, find).unwrap_or_else(|err| {
                     println!("Load error: {}.\nFile possibly corrupted.", err);
                 });
             }
